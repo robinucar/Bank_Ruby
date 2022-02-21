e45452318af0665f9d58ec5a97ccd2b81964ad0f
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-02-21 19:41:40 +0000

    updated date format of test , add date argument to deposit and withdraw methods all test passed

--- week-10/Bank_Ruby/lib/account.rb
+++ week-10/Bank_Ruby/lib/account.rb
@@ -1,16 +1,18 @@
+require 'date'
 class Account
   attr_reader :balance
-
   STARTING_BALANCE = 0
   def initialize
     @balance = STARTING_BALANCE
   end
-  def deposit(amount)
+  def deposit(amount, date = DateTime.now.strftime('%a, %d/%b/%Y'))
     @amount = amount
+    @date = date
     @balance += @amount
   end
-  def withdraw(amount)
+  def withdraw(amount, date = DateTime.now())
     @amount = amount
+    @date = date
     raise 'Payment Failed, your balance is 0' if @balance == 0
     raise 'Payment Failed, your balance is less than witdraw amount' if @balance < @amount
     @balance -= @amount
--- week-10/Bank_Ruby/spec/account_spec.rb
+++ week-10/Bank_Ruby/spec/account_spec.rb
@@ -5,18 +5,18 @@ describe Account do
     expect(account.balance).to eq 0
   end
   it 'balance should  increase by deposit amount' do
-    account.deposit(100, '20/02/2022')
+    account.deposit(100, 'Mon, 21/Feb/2022')
     expect(account.balance).to eq 100
   end
 
   it 'balance should decrease by withdraw amount' do
-    account.deposit(100, '20/02/2022')
-    account.withdraw(25, '21/02/2022')
+    account.deposit(100, 'Mon, 21/Feb/2022')
+    account.withdraw(25, 'Mon, 21/Feb/2022')
     expect(account.balance).to eq 75
   end
 
   it 'should not be able to withdraw money if balance is 0' do
-    expect { account.withdraw(50, '21/02/2022') }.to raise_error('Payment Failed, your balance is 0')
+    expect { account.withdraw(50, 'Mon, 21/Feb/2022') }.to raise_error('Payment Failed, your balance is 0')
   end
 
   it 'should not be able to withdraw money if balance is less than witdraw amount' do
