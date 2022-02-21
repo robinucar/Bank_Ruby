f10e0aee92750f40639e1bfd920ebe5716aa55f7
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-02-21 16:39:04 +0000

    tdd raise an error with witdraw method if balance is 0

--- week-10/Bank_Ruby/spec/account_spec.rb
+++ week-10/Bank_Ruby/spec/account_spec.rb
@@ -5,13 +5,17 @@ describe Account do
     expect(account.balance).to eq 0
   end
   it 'balance should  increase by deposit amount' do
-		account.deposit(100)
-		expect(account.balance).to eq 100
+    account.deposit(100)
+    expect(account.balance).to eq 100
   end
 
-	it 'balance should decrease by withdraw amount' do
-		account.deposit(100)
-		account.withdraw(25)
-		expect(account.balance).to eq 75
-	end
+  it 'balance should decrease by withdraw amount' do
+    account.deposit(100)
+    account.withdraw(25)
+    expect(account.balance).to eq 75
+  end
+
+  it 'should not be able to withdraw money if balance is 0' do
+    expect { account.withdraw(50) }.to raise_error('Payment Failed, your balance is 0')
+  end
 end
