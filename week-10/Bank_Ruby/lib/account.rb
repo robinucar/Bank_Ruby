416c02cbe75305bb5b8464257b18fccaf865efe8
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-02-21 15:47:23 +0000

    tdd deposit method and update balance according tp the deposit amount

--- week-10/Bank_Ruby/lib/account.rb
+++ week-10/Bank_Ruby/lib/account.rb
@@ -1,9 +1,8 @@
 class Account
-    attr_reader :balance
-  
-    STARTING_BALANCE = 0
-    def initialize
-      @balance = STARTING_BALANCE
-    end
+  attr_reader :balance
+
+  STARTING_BALANCE = 0
+  def initialize
+    @balance = STARTING_BALANCE
   end
-  
+end
--- week-10/Bank_Ruby/spec/account_spec.rb
+++ week-10/Bank_Ruby/spec/account_spec.rb
@@ -1,7 +1,11 @@
 require 'account'
 describe Account do
+  let(:account) { Account.new }
   it 'should create an account with 0 balance' do
-    account = Account.new
     expect(account.balance).to eq 0
+  end
+  it 'balance should  increase by depost amount' do
+		account.deposit(100)
+		expect(account.balance).to eq 100
   end
 end
