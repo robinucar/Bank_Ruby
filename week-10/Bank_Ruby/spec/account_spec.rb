d53d26a07d8ce20e737c99f0c6b9600ed21606d5
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-02-22 12:46:33 +0000

    Bank class creates an account

--- week-10/Bank_Ruby/spec/account_spec.rb
+++ week-10/Bank_Ruby/spec/account_spec.rb
@@ -2,9 +2,6 @@ require 'account'
 describe Account do
   let(:account) { Account.new }
   let(:account1) { Account.new(50) }
-  it 'should create an account with 0 balance' do
-    expect(account.balance).to eq 0
-  end
   it 'balance should  increase by deposit amount' do
     account.deposit(100, '21/02/2022')
     expect(account.balance).to eq 100
